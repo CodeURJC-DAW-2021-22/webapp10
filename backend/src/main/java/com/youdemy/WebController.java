f279aebf371cb7e3e3a996b1eddd16c3c06bdf71
Jose Manuel De Pablo Cobo <jm.depablo@alumnos.urjc.es>
2022-02-16 12:15:22 +0100

    header y footer en plantilla aparte, incluidas en las demas plantillas, modificado controlador para enlazar paginas, incluidas clases para entidad Video

--- backend/src/main/java/com/youdemy/WebController.java
+++ backend/src/main/java/com/youdemy/WebController.java
@@ -11,17 +11,54 @@ public class WebController {
 	
 	@Autowired
 	private CourseRepository repository;
+	private VideoRepository videoRepository;
 
 	@GetMapping("/")
 	public String guardarAnuncio(Model model) {
-		
 		Course test = new Course("SQL", "Curso  SQL", "Emiliano", "image.url",100, "tecnologia");
 		repository.save(test);
-
 		return "index";
-		
-		
 	}
+	
+	@GetMapping("/saveVideo")
+	public String saveVideo(Model model) {	
+		Video video = new Video("test","video test","video","video",5,"cocina");
+		videoRepository.save(video);
+		return "index";
+	}
+	
+	@GetMapping("/admin")
+	public String admin(Model model) {		
+		return "admin";	
+	}
+	
+	@GetMapping("/newVideo")
+	public String newVideo(Model model) {		
+		return "newVideo";		
+	}
+	
+	@GetMapping("/login")
+	public String login(Model model) {		
+		return "login";		
+	}
+	
+	@GetMapping("/itemList")
+	public String itemList(Model model) {			
+		return "itemList";		
+	}
+	
+	@GetMapping("/cart")
+	public String cart(Model model) {		
+		return "cart";		
+	}
+	
+	@GetMapping("/checkout")
+	public String checkout(Model model) {		
+		return "checkout";		
+	}
+	
+	@GetMapping("/product")
+	public String product(Model model) {		
+		return "product";		
+	}	
 }
-
-
