ba446dfc90d81d33001e38e3a653c644d04e9f4f
emslmar <emsloth01@gmail.com>
2022-03-14 00:32:56 +0100

    Allow owner of course to delete their property feature

--- backend/src/main/java/com/youdemy/YoudemyApplication.java
+++ backend/src/main/java/com/youdemy/YoudemyApplication.java
@@ -2,9 +2,10 @@ package com.youdemy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class YoudemyApplication {
+public class YoudemyApplication{
 
   public static void main(String[] args) {
     SpringApplication.run(YoudemyApplication.class, args);
