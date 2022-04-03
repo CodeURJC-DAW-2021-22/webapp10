533690786e2d854b50d9505dc40c67ea7b611fe6
Oscar Arranz <oscararranzp@gmail.com>
2022-04-04 00:27:33 +0200

    Add graph data to API

--- backend/src/main/java/com/youdemy/controller/api/UserRestController.java
+++ backend/src/main/java/com/youdemy/controller/api/UserRestController.java
@@ -2,14 +2,22 @@ package com.youdemy.controller.api;
 
 import java.net.URI;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.youdemy.model.Course;
+import com.youdemy.model.CourseBoughtTimes;
+import com.youdemy.service.CourseBoughtTimesService;
+import com.youdemy.service.CourseService;
+import com.youdemy.service.OrderPService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +43,12 @@ public class UserRestController {
 	
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private CourseService courseService;
+
+	@Autowired
+	private CourseBoughtTimesService courseBoughtTimesService;
 	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
@@ -75,7 +89,6 @@ public class UserRestController {
 		if(user.getName().isBlank() || userService.existByEmail(user.getEmail())){
 			return new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
 		} else {
-			
 			user.setEncodedPassword(passwordEncoder.encode(user.getEncodedPassword()));
 			userService.save(user);
 			URI location = fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
@@ -91,10 +104,25 @@ public class UserRestController {
 			return new ResponseEntity<>(null, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-		}	
+		}
 	}
-	
-			
+
+	@RequestMapping("/{id}/graph")
+	public ResponseEntity<List<CourseBoughtTimes>> graph(@PathVariable long id) {
+		if (userService.exist(id)) {
+			User user = userService.findById(id).get();
+
+			if (user.getRoles().contains("TEACHER")) {
+				ArrayList<CourseBoughtTimes> coursesBoughtTimes =
+						new ArrayList<>(courseBoughtTimesService.getCourseBoughtTimes(user));
+				URI location = fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+				return ResponseEntity.created(location).body(coursesBoughtTimes);
+			}
+		}
+
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
 	
 }
 
