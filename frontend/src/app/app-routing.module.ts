357247b94d82afdabee245520369799d525ee1ca
fiorellaV <fiorellavictoria70@gmail.com>
2022-04-22 16:46:07 +0200

    Create header and footer components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CoursesListComponent } from './pages/CoursesPage/courses-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'courses', component: CoursesListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/pages/CoursesPage/courses-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Course } from 'src/models/course.model';
+import { CourseService } from 'src/services/course.service';
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: 'courses-list.component.html',
+})
+export class CoursesListComponent {
+  courses: Course[] = [];
+
+  ngOnInit() {
+
+  }
+
+  constructor(private courseService: CourseService) {}
+
+  loadCourses() {
+    return this.courseService.getCourses().subscribe((data: Course[]) => {
+      this.courses = data;
+    });
+  }
+}
