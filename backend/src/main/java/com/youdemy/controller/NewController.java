ba516bf6aacc35d55e25f87d2b436a1fe59fe185
Oscar Arranz <oscararranzp@gmail.com>
2022-04-26 19:25:50 +0200

    Add ant pattern matcher

--- backend/src/main/java/com/youdemy/controller/NewController.java
+++ backend/src/main/java/com/youdemy/controller/NewController.java
@@ -1,17 +1,7 @@
-
 package com.youdemy.controller;
 
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.Optional;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class NewController {
