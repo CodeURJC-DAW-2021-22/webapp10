e7fcf855b724b7aa62e44181914b9fbd5a78c77a
Oscar Arranz <oscararranzp@gmail.com>
2022-04-26 19:31:04 +0200

    Refactor NewController to FrontEndController, fix application.properties

--- backend/src/main/java/com/youdemy/controller/FrontEndController.java
+++ backend/src/main/java/com/youdemy/controller/FrontEndController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NewController {
+public class FrontEndController {
 	@GetMapping({"/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
 	public String redirect() {
 		return "forward:/new/index.html";
