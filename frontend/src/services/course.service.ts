e20715ce4478cd5ba975578590f4d8f5aa00cb10
Oscar Arranz <oscararranzp@gmail.com>
2022-04-26 19:34:44 +0200

    Add urlTemplate function to course.service.ts

--- frontend/src/services/course.service.ts
+++ frontend/src/services/course.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from 'src/models/course.model';
 
+const urlTemplate = (route: string) => `https://localhost:8080${route}`;
+
 @Injectable({ providedIn: 'root' })
 export class CourseService {
   constructor(private httpClient: HttpClient) {}
 
   getCourses(): Observable<Course[]> {
-    return this.httpClient.get<Course[]>('https://localhost:8080/api/courses/');
+    return this.httpClient.get<Course[]>(urlTemplate('/api/courses'));
   }
 }
